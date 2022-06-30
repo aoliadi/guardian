5bc69cd95fcffce87673189a802d1d735f9fe023
aoliadi <aoliadi1998@gmail.com>
2022-06-30 02:10:19 +0100

    looks broken

--- src/Components/Hooks/useFetch.js
+++ src/Components/Hooks/useFetch.js
@@ -1,18 +1,23 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+// const useFetch = ({ api, params, category }) => {
 const useFetch = (url, params) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [err, setErr] = useState(null);
 
+  // console.log(category);
+  // console.log(params);
+
   useEffect(() => {
     axios
+      // .get(api, {
       .get(url, {
         params,
       })
       .then((res) => {
-        console.log(res);
+        // console.log(res);
         setData(res.data);
         // setData(res[theData]);
         setLoading(false);
@@ -22,8 +27,8 @@ const useFetch = (url, params) => {
         setErr(err.message);
         setLoading(true);
       });
-  }, []);
-
+  }, [url]);
+  // console.log(data);
   return { data, loading, err };
 };
 
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,24 +1,57 @@
-// import styles from "../css/article.module.css";
+import article from "../css/article.module.css";
 import general from "../css/general.module.css";
-// import News from "./News";
 import Test from "./Test";
 import useFetch from "./Hooks/useFetch";
-import useRandom from "./Hooks/useRandom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Main() {
-  // const category = ["sports", "politics", "entertainment"];
-  const theGuardianAPI = `https://content.guardianapis.com/search`;
-  const theGuardianParams = {
-    "api-key": "db0f505c-2e61-417a-b77b-925796f744d7",
-    "page-size": 25,
+  // const [category, setCategory] = useState([
+  const category = ["sports", "politics", "entertainment"];
+  const [newsState, setnewsState] = useState({
+    api: `https://content.guardianapis.com/search`,
+    params: {
+      "api-key": "db0f505c-2e61-417a-b77b-925796f744d7",
+      "page-size": 20,
+    },
+  });
+  // newsState.api = "http://localhost:8000/response";
+  const unsplashState = {
+    api: `https://api.unsplash.com/photos`,
+    params: {
+      client_id: "khjyPe4b-GQR9I_WB0jw4cZDN_inbfU685jsV-AtKeI",
+    },
   };
-  const { data, loading, err } = useFetch(theGuardianAPI, theGuardianParams);
+  const {
+    data: newsData,
+    loading: newsIsLoading,
+    err: newsFetchErr,
+    // } = useFetch(newsState);
+  } = useFetch(newsState.api, newsState.params);
+  const { data: imgData, err: imgFetchErr } = useFetch(
+    unsplashState.api,
+    unsplashState.params
+  );
+  const imgUrls = imgData && imgData.map((item) => item.urls.small_s3);
+
+  // console.log(newsData);
 
   return (
     <main className={general.container}>
-      {err && <h1 className="">{err}</h1>}
-      {loading || <Test newsArr={data.response.results} />}
+      <div className={article.wrapper}>
+        {newsFetchErr && <h1 className="">{newsFetchErr}</h1>}
+        {newsIsLoading ||
+          category.map((item, index) => {
+            console.log(newsData.results);
+            return (
+              <Test
+                key={index}
+                newsArr={newsData.results}
+                category={item}
+                imgUrls={imgUrls}
+              />
+            );
+          })}
+      </div>
     </main>
   );
 }
