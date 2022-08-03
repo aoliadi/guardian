b391646e1715e57cc3c1f30cfe1406f5e9b3f503
aoliadi <aoliadi1998@gmail.com>
2022-08-03 14:37:26 +0100

    done with navbar styling

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,55 @@
+import navbar from "../css/navbar.module.css";
+
 function Navbar() {
   return (
-    <header className="">
-      <h1>The navbar heading</h1>
-      <nav className=""></nav>
-    </header>
+    <nav className={navbar.container}>
+      <ul className={navbar.list_container}>
+        <li className={navbar.list_logo}>
+          <a href="" className={navbar.link}>
+            N.
+          </a>
+        </li>
+        <li className={navbar.list_item}>
+          <a href="" className={navbar.link}>
+            Business
+          </a>
+        </li>
+        <li className={navbar.list_item}>
+          <a href="" className={navbar.link}>
+            Lifestyle
+          </a>
+        </li>
+        <li className={navbar.list_item}>
+          <a href="" className={navbar.link}>
+            Sports
+          </a>
+        </li>
+        <li className={navbar.list_item}>
+          <a href="" className={navbar.link}>
+            Politics
+          </a>
+        </li>
+        <li className={navbar.list_item}>
+          <svg
+            onClick={() => {
+              console.log("search!");
+            }}
+            className={navbar.search_icon}
+            aria-labelledby="title desc"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 19.9 19.7"
+          >
+            <title id="title">search</title>
+            <desc id="desc">A magnifying glass icon.</desc>
+            <g className={navbar.search_path} fill="none" stroke="#848F91">
+              <path strokeLinecap="square" d="M18.5 18.3l-5.4-5.4" />
+              <circle cx={8} cy={8} r={7} />
+            </g>
+          </svg>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
